117f8b8e1edbce8cf370b589815c14969de4c159
Anna Parulava <a554156554156@gmail.com>
2022-09-23 18:13:58 +0300

    Добавление макетов приложения.

--- app/src/main/java/com/example/mobileupllc/MainActivity.kt
+++ app/src/main/java/com/example/mobileupllc/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mobileupllc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.mobileupllc.fragments.CryptocurrenciesListFragment
+import com.example.mobileupllc.fragments.ErrorMessageFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(CryptocurrenciesListFragment.newInstance(), "cryp_fragment")
+    }
+
+    private fun loadFragment(fragment: Fragment, tag: String) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.host_fragment, fragment, tag)
+        ft.commit()
     }
 }
